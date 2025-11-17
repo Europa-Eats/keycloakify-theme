635c49bb3e04659278934c657163b59f7f4c8ed7
eliaskounakas <elias.kounakas@proton.me>
2025-11-17 14:51:59 +0100

    create basic login page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,7 +3,9 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
+import './main.css';
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -36,5 +38,7 @@ export default function KcPage(props: { kcContext: KcContext }) {
         </Suspense>
     );
 }
+
+// ...
 
 const classes = {} satisfies { [key in ClassKey]?: string };
